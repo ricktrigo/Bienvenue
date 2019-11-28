14050e069a1aae4ecc56bd965412febb9ae84a83
Ricardo Trigo <ricardo.and@hotmail.es>
2019-11-28 09:08:40 +0100

    ajout feature 3, ajout majuscule nom feature1 @ricktrigo

--- tests/features/test_bienvenue.py
+++ tests/features/test_bienvenue.py
@@ -2,7 +2,7 @@ import Bienvenue
 
 
 def test_hello_1_nom():
-    assert  Bienvenue.hello("bob")=="Hello, bob"
+    assert  Bienvenue.hello("bob")=="Hello, Bob"
     assert Bienvenue.hello(" ") == "Hello, my friend"
     assert Bienvenue.hello("") == "Hello, my friend"
 
@@ -10,3 +10,6 @@ def test_hello_1_nom():
 def test_estvide():
     assert Bienvenue.estvide("    ") == True
     assert Bienvenue.estvide("") == True
+
+def test_cris():
+    assert Bienvenue.hello("JERRY") == "HELLO, JERRY!"
