c85b70313e7cd0ce7334f6f0d6b41e2cd94cb352
Ricardo Trigo <ricardo.and@hotmail.es>
2019-12-03 08:14:42 +0100

    Tests feature5 sans modification de la fonction hello passés

--- tests/features/test_bienvenue.py
+++ tests/features/test_bienvenue.py
@@ -16,6 +16,11 @@ def test_cris():
 
 
 def test_2_input():
-    assert Bienvenue.hello2("Amy,bob")=="Hello, Amy, Bob"
-    assert Bienvenue.hello2(" amy,bob") == "Hello, Amy, Bob"
-    assert Bienvenue.hello2("bob, amy") == "Hello, Bob, Amy"
+    assert Bienvenue.hellon("Amy,bob")=="Hello, Amy, Bob"
+    assert Bienvenue.hellon(" amy,bob") == "Hello, Amy, Bob"
+    assert Bienvenue.hellon("bob, amy") == "Hello, Bob, Amy"
+
+def test_n_input():
+    assert Bienvenue.hellon("Amy, Bob,Jerry")=="Hello, Amy, Bob, Jerry"
+    assert Bienvenue.hellon(" amy,bob,jerry") == "Hello, Amy, Bob, Jerry"
+    assert Bienvenue.hellon("bob, amy, jerry") == "Hello, Bob, Amy, Jerry"
